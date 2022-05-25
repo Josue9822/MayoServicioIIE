c86bfdec92a893cf6f8da3be3f70c79de4f2d9f5
Josue Perez <jos22_98@hotmail.com>
2022-05-24 21:39:06 -0500

    Conexion con bd

--- src/main/java/com/idat/MayoServicioPrueba/service/ProductoServiceImpl.java
+++ src/main/java/com/idat/MayoServicioPrueba/service/ProductoServiceImpl.java
@@ -16,32 +16,32 @@ public class ProductoServiceImpl implements ProductoService {
 	
 	@Override
 	public void guardarProducto(Productos producto) {
-		repository.guardarProducto(producto);
+		repository.save(producto);
 
 	}
 
 	@Override
 	public void actualizarProducto(Productos producto) {
-		repository.actualizarProducto(producto);
+		repository.saveAndFlush(producto);
 
 	}
 
 	@Override
 	public void eliminarProducto(Integer id) {
-		repository.eliminarProducto(id);
+		repository.deleteById(id);
 
 	}
 
 	@Override
 	public List<Productos> listarProductos() {
 		// TODO Auto-generated method stub
-		return repository.listarProductos();
+		return repository.findAll();
 	}
 
 	@Override
 	public Productos obtenerProductoId(Integer id) {
 		// TODO Auto-generated method stub
-		return repository.obtenerProductoId(id);
+		return repository.findById(id).orElse(null);
 	}
 
 }
