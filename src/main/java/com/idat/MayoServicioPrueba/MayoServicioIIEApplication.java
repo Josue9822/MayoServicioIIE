064ddb5d4663d12717617ceda3c828dfada4d451
Josue Perez <jos22_98@hotmail.com>
2022-06-21 19:31:27 -0500

    cambios realizados

--- src/main/java/com/idat/MayoServicioPrueba/MayoServicioIIEApplication.java
+++ src/main/java/com/idat/MayoServicioPrueba/MayoServicioIIEApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RepasoClasesApplication {
+public class MayoServicioIIEApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RepasoClasesApplication.class, args);
+		SpringApplication.run(MayoServicioIIEApplication.class, args);
 	}
 
 }
